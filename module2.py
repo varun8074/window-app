18381a5809040e1b5ebbe9c01e1196179c8c67f5
varun <rvarunraj_ps@nitw.ac.in>
2025-09-12 11:51:17 +0530

    update 7 to update 6

--- module2.py
+++ module2.py
@@ -146,32 +146,21 @@ class MinimizerWidget(QWidget):
     # ----------------- Hotkey -----------------
     def bind_hotkey(self):
         if not self._hotkey_bound:
-            try:
-                # Save the hotkey id so we can unbind it safely later
-                self._hotkey_id = keyboard.add_hotkey(
-                    "numlock", self._on_hotkey_triggered, suppress=False
-                )
-                self._hotkey_bound = True
-                self._update_status()
-            except Exception as e:
-                print("Failed to bind hotkey:", e)
+            keyboard.add_hotkey("numlock", self._on_hotkey_triggered)
+            self._hotkey_bound = True
+            self._update_status()
 
     def unbind_hotkey(self):
         if self._hotkey_bound:
             try:
-                keyboard.remove_hotkey(self._hotkey_id)
-            except Exception as e:
-                print("Failed to unbind hotkey:", e)
+                keyboard.remove_hotkey("numlock")
+            except KeyError:
+                pass
             self._hotkey_bound = False
             self._update_status()
 
     def _on_hotkey_triggered(self):
-        try:
-            print("NumLock hotkey pressed")
-            self.run_worker_once()
-        except Exception as e:
-            # Prevent crash if something goes wrong
-            print("Hotkey callback error:", e)
+        self.run_worker_once()
 
     def run_worker_once(self):
         if self._worker_running:
@@ -209,10 +198,7 @@ class MinimizerWidget(QWidget):
             self.close_edit.setText(app)
 
     def closeEvent(self, e):
-        # Only unbind if the entire app is quitting, 
-        # not when just switching modules in main.py
-        from PyQt6.QtWidgets import QApplication
-        if QApplication.instance().closingDown() and self._hotkey_bound:
+        if self._hotkey_bound:
             self.unbind_hotkey()
         return super().closeEvent(e)
 
